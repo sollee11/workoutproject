c2fdc12a5fd9ad2f6793e88d92f6535e89c832f5
sol <jgodsoul@icloud.com>
2024-12-11 11:30:04 +0900

    2024.12.10 update

---
+++ workoutProject/src/main/java/org/zerock/workoutproject/WorkoutProjectApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.workoutproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutProjectApplication.class, args);
+    }
+
+}
---
+++ workoutProject/src/test/java/org/zerock/workoutproject/WorkoutProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.workoutproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
