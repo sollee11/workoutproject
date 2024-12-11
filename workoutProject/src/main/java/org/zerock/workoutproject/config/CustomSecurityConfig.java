b2d2805c7abc02f2adbe0863ab6474f3ba259c30
sol <jgodsoul@icloud.com>
2024-12-11 11:44:34 +0900

    2024.12.11 공통파일 업데이트

---
+++ workoutProject/src/main/java/org/zerock/workoutproject/config/CustomSecurityConfig.java
@@ -0,0 +1,62 @@
+package org.zerock.workoutproject.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Log4j2
+@Configuration
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class CustomSecurityConfig {
+
+  //스프링 시큐리티 기본 설정
+  // 모든 페이지에 로그인이 필요, 로그인 성공시 / 경로로 이동
+
+  // 패스워드 암호화 방식 설정
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
+  //스프링 시큐리티 커스텀 설정 메서드
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    log.info("---------------------configure-----------------------");
+//    // form태그를 사용한 로그인
+//    http.formLogin().loginPage("/member/login");
+//    // CSRF 끄기 설정
+//    http.csrf().disable();
+//    // 자동로그인 설정
+//    http.rememberMe()
+//        .key("12345678")
+//        .tokenRepository(persistentTokenRepository())
+//        .userDetailsService(userDetailsService)
+//        .tokenValiditySeconds(60*60*24*30);
+//    // 403에러 예외처리
+//    http.exceptionHandling().accessDeniedHandler(accessDeniedHandler());
+//    // oauth2 로그인 페이지 설정
+//    http.oauth2Login()
+//        .loginPage("/member/login")
+//        .successHandler(authenticationSuccessHandler());
+
+    return http.build();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ workoutProject/src/main/java/org/zerock/workoutproject/config/CustomServletConfig.java
@@ -0,0 +1,28 @@
+package org.zerock.workoutproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CustomServletConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //addResourceHandler("URL 경로").addResourceLocations("실제 파일 경로")
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/static/assets/");
+
+        // template 폴더의 파일 불러오기 설정
+//        registry.addResourceHandler("/template/**").addResourceLocations("classpath:/template/");
+        // 외부 c드라이브의 download 폴더의 파일 불러오기 설정
+//        registry.addResourceHandler("/download/**").addResourceLocations("file:///c:/download/");
+    }
+}
