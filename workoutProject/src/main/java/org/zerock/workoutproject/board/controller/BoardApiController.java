9d4eef88edcff7bb62ca1c803fc9e717b9ce9c12
park\sub <pjoonwoo111@gmail.com>
2024-12-27 16:55:54 +0900

    api컨트롤러 수정

--- workoutProject/src/main/java/org/zerock/workoutproject/board/controller/BoardApiController.java
+++ workoutProject/src/main/java/org/zerock/workoutproject/board/controller/BoardApiController.java
@@ -31,31 +31,5 @@ public class BoardApiController {
         return ResponseEntity.ok(viewCounts);
     }
 
-    // 게시글 CRUD API
-    @GetMapping("/{bno}")
-    public ResponseEntity<BoardDTO> getBoard(@PathVariable Long bno) {
-        BoardDTO board = boardService.readOne(bno);
-        return ResponseEntity.ok(board);
-    }
 
-    @PostMapping
-    public ResponseEntity<Long> createBoard(@Valid @RequestBody BoardDTO boardDTO) {
-        Long bno = boardService.register(boardDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(bno);
-    }
-
-    @PutMapping("/{bno}")
-    public ResponseEntity<Long> updateBoard(
-            @PathVariable Long bno,
-            @Valid @RequestBody BoardDTO boardDTO) {
-        boardDTO.setBno(bno);
-        Long updatedBno = boardService.modify(boardDTO);
-        return ResponseEntity.ok(updatedBno);
-    }
-
-    @DeleteMapping("/{bno}")
-    public ResponseEntity<Void> deleteBoard(@PathVariable Long bno) {
-        boardService.remove(bno);
-        return ResponseEntity.noContent().build();
-    }
 }
